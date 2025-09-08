99004ff569a02693ddcef9c0133a45d67e4e16ec
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-09-08 23:44:20 +0600

    user authenticate middleware add

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+const corsOption = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+};
+app.use(cors(corsOption));
 
 // MongoDB Connection
 const uri = process.env.MONGO_URI;
@@ -21,6 +26,21 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// middleware
+const authenticate = (req, res, next) => {
+  const token = req.cookie.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "No token" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid token" });
+  }
+};
 async function run() {
   try {
     const usersCollection = client.db("islamic-quotes").collection("users");
@@ -68,6 +88,20 @@ async function run() {
         if (!isPasswordValid) {
           return res.status(401).json({ message: "Invalid password" });
         }
+
+        // create token
+        const token = jwt.sign(
+          { id: user._id, email: user.email },
+          process.env.JWT_SECRET,
+          { expiresIn: "1h" }
+        );
+        // set cookie
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "lax",
+          maxAge: 60 * 60 * 1000,
+        });
         res.status(200).json({
           message: "Login successful",
           user: {
