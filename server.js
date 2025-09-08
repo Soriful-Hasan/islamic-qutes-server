cb54c95462ab43ec38d67dc3afae286ffb4dd196
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-09-08 16:01:35 +0600

    user login route add

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
 const app = express();
@@ -26,7 +27,7 @@ async function run() {
 
     app.post("/register", async (req, res) => {
       try {
-        const { name, email, password, photo } = req.body;
+        // const { name, email, password, photo } = req.body;
 
         const existUser = await usersCollection.findOne({ email });
         if (existUser) {
@@ -48,6 +49,34 @@ async function run() {
         res
           .status(201)
           .json({ message: "User registered successfully", user: newUser });
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
+
+    app.post("/login", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+        console.log(email, password);
+        // check if user exist
+        const user = await usersCollection.findOne({ email });
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+        // compare password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          return res.status(401).json({ message: "Invalid password" });
+        }
+        res.status(200).json({
+          message: "Login successful",
+          user: {
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            photo: user.photo,
+          },
+        });
       } catch (error) {
         res.status(500).json({ message: error.message });
       }
